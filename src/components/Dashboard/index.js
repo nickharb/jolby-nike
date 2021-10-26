a7759f616babe210701632aadedb988bddfc7ff0
Nick Harbaugh <nickharb@nickssqlywiggly.lan>
2021-10-26 16:38:58 -0500

    dashboard base styles

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,73 +1,82 @@
 import React, { Component } from 'react';
 import { Header, Nav } from '../';
 import './style.css';
+import person from './img/person.svg';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql
 } from "@apollo/client";
 
 
 const PORTFOLIOS = gql`
-  query getPortfolios {
-    portfolios(limit:5) {
-      id
-      name
-      projects {
-        id
-        name
-        people{
-          id
-        }
-      }
-    } 
-  }
+    query getPortfolios {
+        portfolios(limit:5) {
+            id
+            name
+            projects {
+                id
+                name
+                abbreviation
+                portfolioName
+                people {
+                    id
+                }
+            }
+        } 
+    }
 `;
 
 function Dashboard() {
-  const { loading, error, data } = useQuery(PORTFOLIOS);
-  if (loading) return <p>Loading pls wait thank you...</p>;
-  if (error) return <p>{error}</p>;
-  console.log(data)
+    const { loading, error, data } = useQuery(PORTFOLIOS);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>{error}</p>;
 
-   let teams = data.portfolios.map(({ id, name, projects }) => (
-      <div key={id} className="panel">
-        <h1>
-          {name}
-        </h1>
-        <div>
-          {projects.map(function (project) {
-                  return (
-                    <div key={project.id}>
-                      <h4></h4>
-                      <span>{project.name}</span>
-                      <p>{project.people.length}</p>
+    let teams = data.portfolios.map(({ id, name, projects }) => (
+        <div key={id} className="panel">
+            <div className="orb"></div>
+            <h1 className="panel-header">
+                {name}
+            </h1>
+
+            <div >
+            {projects.map(function (project) {
+                return (
+                    <div className="project-wrapper" key={project.id}>
+                        <div className="project-left">
+                            <span>{project.abbreviation}</span>
+                        </div>
+                        <div className="project-right">
+                            <h2>{project.name}</h2>
+                            <h3>{project.portfolioName}</h3>
+                            <div className="people-counter">
+                                <img src={person} alt="Person" /><p>{project.people.length}</p>
+                            </div>
+                            {/*<p>{project.people.length}</p>*/}
+                        </div>
                     </div>
-                  );
-          })}
+                );
+            })}
+            </div>
         </div>
-      </div>
     ));
-    console.log(teams)
-
-
 
     return (
-     <>
-     <Header></Header>
-     <Nav></Nav>
-     <div className="Dashboard content-wrapper">
-          {teams}
-      </div>
-     </>
+        <>
+        <Header></Header>
+        <Nav></Nav>
+        <div className="Dashboard content-wrapper">
+                {teams}
+        </div>
+         </>
     );
 }
 
