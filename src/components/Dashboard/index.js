7dab6bce1234d995e6905880d5a607f9a3993338
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2021-11-17 17:16:21 -0500

    adds different p5 sketches per swirl

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { Header, Nav } from '../';
 import './style.css';
+import sketch from'./p5/sketch1.js'
+import sketch2 from'./p5/sketch2.js'
+import sketch3 from'./p5/sketch3.js'
+import sketch4 from'./p5/sketch4.js'
+import sketch5 from'./p5/sketch5.js'
 
 import Sketch from 'react-p5';
 import { ReactP5Wrapper } from "react-p5-wrapper";
@@ -46,116 +51,6 @@ const PORTFOLIOS = gql`
 
 function Dashboard() {
 
-    let height = 230;
-    let width = 230;
-
-    let g;
-    let m;
-    let t1 = 0;
-    let t2 = 0;
-    let c_points = [];
-
-    let orbSize = 40;
-
-    let color1;
-    let color2;
-    let color0;
-
-    let lineCount;
-    let lineSpeed;
-
-    let startColor = 255;
-    let speedAnimate = 0.005;
-
-    const setup = (p5, canvasParentRef) => {
-        p5.createCanvas(width, height).parent(canvasParentRef);
-
-        let p = p5.random(8, 20);
-
-        for (let x = 1; x <= 360; x +=8) { // number of points projects
-            c_points.push(p5.radians(x));
-        }
-
-        lineCount = 20;
-        lineSpeed = 0.003;
-        color0 = p5.color(0,0,0);
-        color1 = p5.color(12, 55, 176); // dark blue
-        color2 = p5.color(158, 243, 243, 100); // light blue
-    }
-
-    const draw = (p5) => {
-        p5.blendMode(p5.BLEND);
-        p5.background(0);
-        p5.blendMode(p5.ADD);
-        drawBg(p5);
-        p5.colorMode(p5.RGB, p5.TWO_PI);
-    }
-
-    const drawBg = (p5) => {
-        p5.strokeWeight(1.5);
-        p5.noFill();
-        t1 = 0;
-        t2 = t2 + lineSpeed; //speed
-
-        for (let f = 0; f < lineCount; f++) { //number of lines
-            t1 = t1 + 0.03; //uniformity of lines
-            // g = f * 0.01; //gradient radius
-            g = f * 0.01; //gradient radius
-            // m = g*2;
-            m = g * 10;
-
-            let c = p5.lerpColor(color1, color2, g);
-            let h = p5.lerpColor(color0, c, m);
-
-            p5.stroke(h);
-
-            p5.beginShape();
-
-            let r = orbSize; // size
-            let a = 0;
-            let n = 0;
-
-            a = c_points[0];
-            n = p5.map(p5.noise(t1, t2, a), 0, 1,1, 2); 
-            const [x0, y0] = circle_point(width / 2, height / 2, n * (r + f), a);
-            p5.curveVertex(x0, y0);
-
-
-            a = c_points[1];
-            n = p5.map(p5.noise(t1, t2, a), 0, 1, 1, 2);
-            const [x1, y1] = circle_point(width / 2, height / 2, n * (r + f), a);
-            p5.curveVertex(x1, y1);
-
-            a = c_points[2];
-            n = p5.map(p5.noise(t1, t2, a), 0, 1, 1, 2);
-            const [x2, y2] = circle_point(width / 2, height / 2, n * (r + f), a);
-            p5.curveVertex(x2, y2);
-
-            for (let i = 0; i < c_points.length; i++) {
-                if (i > 3) {
-                    a = c_points[i];
-                    n = p5.map(p5.noise(t1, t2, a), 0, 1, 1, 2);
-                    const [x, y] = circle_point(width / 2, height / 2, n * (r + f), a);
-                    p5.curveVertex(x, y);
-                }
-            }
-
-            p5.curveVertex(x0, y0);
-            p5.curveVertex(x1, y1);
-            p5.curveVertex(x2, y2);
-
-            p5.endShape();
-
-        } // for loop end
-      
-
-        function circle_point(cx, cy, r, a) {
-            const x = cx + r * p5.cos(a);
-            const y = cy + r * p5.sin(a);
-            return [x, y];
-        }
-    } // end drawBg
-
 
 
 
@@ -166,7 +61,21 @@ function Dashboard() {
     let teams = data.portfolios.map(({ id, name, projects }, index) => (
         <div key={id} className="panel">
             <div className="orb">
-                <Sketch setup={setup} draw={draw} />
+             {index == 0 &&
+                <ReactP5Wrapper sketch={sketch} />
+             }
+             {index == 1 &&
+                <ReactP5Wrapper sketch={sketch2} />
+             }
+             {index == 2 &&
+                <ReactP5Wrapper sketch={sketch3} />
+             }
+             {index == 3 &&
+                <ReactP5Wrapper sketch={sketch4} />
+             }
+             {index == 4 &&
+                <ReactP5Wrapper sketch={sketch5} />
+             }
             </div>
             <h1 className="panel-header">
                 {name}
---
+++ src/components/Dashboard/p5/sketch2.js
@@ -0,0 +1,124 @@
+import React from "react";
+import { ReactP5Wrapper } from "react-p5-wrapper";
+
+function sketch2 (p) {
+
+    let height = 230;
+    let width = 230;
+
+    let g;
+    let m;
+    let t1 = 0;
+    let t2 = 0;
+    let c_points = [];
+
+    let orbSize = 40;
+
+    let color1;
+    let color2;
+    let color0;
+
+    let lineCount;
+    let lineSpeed;
+
+    let startColor = 255;
+    let speedAnimate = 0.005;
+
+  p.setup = (canvasParentRef) => {
+        
+        p.createCanvas(width, height).parent(canvasParentRef);
+
+        for (let x = 1; x <= 360; x +=8) { // number of points projects
+            c_points.push(p.radians(x));
+        }
+
+        lineCount = 20;
+        lineSpeed = 0.003;
+        color0 = p.color(0,0,0);
+        color1 = p.color(176, 55, 12); // dark blue
+        color2 = p.color(243, 243, 158, 100); // light blue
+  }
+
+  p.draw = function (){
+       p.blendMode(p.BLEND);
+        p.background(0);
+        p.blendMode(p.ADD);
+        drawBg();
+        p.colorMode(p.RGB, p.TWO_PI);
+       
+  } 
+
+
+ const drawBg = function(){
+        p.strokeWeight(1.5);
+        p.noFill();
+        t1 = 0;
+        t2 = t2 + lineSpeed; //speed
+
+        for (let f = 0; f < lineCount; f++) { //number of lines
+            t1 = t1 + 0.03; //uniformity of lines
+            // g = f * 0.01; //gradient radius
+            g = f * 0.01; //gradient radius
+            // m = g*2;
+            m = g * 10;
+
+            let c = p.lerpColor(color1, color2, g);
+            let h = p.lerpColor(color0, c, m);
+
+            p.stroke(h);
+
+            p.beginShape();
+
+            let r = orbSize; // size
+            let a = 0;
+            let n = 0;
+
+            a = c_points[0];
+            n = p.map(p.noise(t1, t2, a), 0, 1,1, 2); 
+            const [x0, y0] = circle_point(width / 2, height / 2, n * (r + f), a);
+            p.curveVertex(x0, y0);
+
+
+            a = c_points[1];
+            n = p.map(p.noise(t1, t2, a), 0, 1, 1, 2);
+            const [x1, y1] = circle_point(width / 2, height / 2, n * (r + f), a);
+            p.curveVertex(x1, y1);
+
+            a = c_points[2];
+            n = p.map(p.noise(t1, t2, a), 0, 1, 1, 2);
+            const [x2, y2] = circle_point(width / 2, height / 2, n * (r + f), a);
+            p.curveVertex(x2, y2);
+
+            for (let i = 0; i < c_points.length; i++) {
+                if (i > 3) {
+                    a = c_points[i];
+                    n = p.map(p.noise(t1, t2, a), 0, 1, 1, 2);
+                    const [x, y] = circle_point(width / 2, height / 2, n * (r + f), a);
+                    p.curveVertex(x, y);
+                }
+            }
+
+            p.curveVertex(x0, y0);
+            p.curveVertex(x1, y1);
+            p.curveVertex(x2, y2);
+
+            p.endShape();
+
+        } // for loop end
+      
+
+        function circle_point(cx, cy, r, a) {
+            const x = cx + r * p.cos(a);
+            const y = cy + r * p.sin(a);
+            return [x, y];
+        }
+    }
+
+};
+
+
+export default sketch2
+
+
+
+
