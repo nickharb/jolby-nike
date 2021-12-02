e6f1c604187a1c8000d067ca739f9b7e55a0f73a
Sam Alexander <sxalexander@gmail.com>
2021-12-01 17:23:53 -0800

    Adds actioncable library and example using newPerson subscription

--- src/index.js
+++ src/index.js
@@ -3,25 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// include the actioncable javascript libraries
+import ActionCable from 'actioncable';
+import ActionCableLink from 'graphql-ruby-client/subscriptions/ActionCableLink';
+
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   useQuery,
+  useSubscription,
   gql
 } from "@apollo/client";
 import { HttpLink } from '@apollo/client';
 import { split } from '@apollo/client';
-import { WebSocketLink } from '@apollo/client/link/ws';
+// import { WebSocketLink } from '@apollo/client/link/ws';
 import { getMainDefinition } from '@apollo/client/utilities';
 
+// ActionCable requests have a standard protocol.  
+// All apollo requests need to be wrapped in the correct messaging 
+// format in order to be processed correctly.
 
-const wsLink = new WebSocketLink({
-  uri: 'wss://webapp-tn6y.onrender.com/cable',
-  options: {
-    reconnect: true,
-  }
-});
+const createActionCableLink = () => {
+  const cable = ActionCable.createConsumer(`wss://webapp-tn6y.onrender.com/cable`);
+  return new ActionCableLink({ cable });
+};
+
 const httpLink = new HttpLink({
   uri: 'https://webapp-tn6y.onrender.com/api/graphql',
   options: {
@@ -37,7 +45,7 @@ const link = split(
       operation === 'subscription'
     );
   },
-  wsLink,
+  createActionCableLink(), // if the operation type is subscription, use the actioncable endpoint
   httpLink
 );
 
@@ -51,11 +59,33 @@ const client = new ApolloClient({
 //   cache: new InMemoryCache()
 // });
 
+
+// injecting an example subscription GraphQL query here
+const PERSON_SUBSCRIPTION = gql`
+subscription {
+  newPerson {
+    id
+    name
+  }
+}`;
+
+// subscribe and return
+function LatestPerson({ postID }) {
+  const { data, loading } = useSubscription(
+    PERSON_SUBSCRIPTION,
+    { variables: {  } }
+  );
+  
+  return <h4>{!loading && console.log(data)}</h4>;
+}
+
+
 /* And we wrap the app in Apolloprovider so all the components/pages have access to the api */
 ReactDOM.render(
   <React.StrictMode>
 	  <ApolloProvider client={client}>
 	    <App />
+      <LatestPerson />
 	  </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
